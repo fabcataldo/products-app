7ccd3399ade3eb8c8cd76d39f881e02d8c0ebb5c
Fabio <fabioalbertocataldo@gmail.com>
2025-10-16 17:11:25 -0300

    add: tanstack/react-query logic base implementation in layout root app page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,22 @@
 import { useColorScheme } from '@/presentation/theme/hooks/use-color-scheme';
 import { useThemeColor } from '@/presentation/theme/hooks/use-theme-color';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
 import { useFonts } from "expo-font";
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false
+      }
+    }
+  }
+);
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -31,18 +38,16 @@ export default function RootLayout() {
         flex: 1
       }}
     >
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack
-          screenOptions={{
-            headerShown: false
-          }}
-        >
-          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack
+            screenOptions={{
+              headerShown: false
+            }}
+          >
+          </Stack>
+        </ThemeProvider>      
+      </QueryClientProvider>
     </GestureHandlerRootView>
-    
   );
 }
