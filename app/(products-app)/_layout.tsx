4c9b5ad7db6a5a392369ff51417e53679ee084b8
Fabio <fabioalbertocataldo@gmail.com>
2025-10-16 17:11:34 -0300

    add: product/id route stack

--- app/(products-app)/_layout.tsx
+++ app/(products-app)/_layout.tsx
@@ -49,6 +49,12 @@ const CheckAuthenticationLayout = () => {
                     headerLeft: () => <LogoutIconButton/>
                 }}
             />
+            <Stack.Screen
+                name='product/[id]'
+                options={{
+                    title: 'Producto',
+                }}
+            />
         </Stack>
     )
 
