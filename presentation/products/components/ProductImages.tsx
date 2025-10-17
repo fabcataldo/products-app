7e22c94e5e09b1e0eea11d0e0fe93fdf2713055b
Fabio <fabioalbertocataldo@gmail.com>
2025-10-17 17:30:21 -0300

    fix: productimage component styles

--- presentation/products/components/ProductImages.tsx
+++ presentation/products/components/ProductImages.tsx
@@ -8,7 +8,12 @@ interface Props {
 const ProductImages = ({images}: Props) => {
 
   if(images.length === 0) {
-    <View>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center'
+      }}
+    >
       <Image source={ require('../../../assets/images/no-product-image.png') }
       style={{width: 300, height: 300}}/>
     </View>
